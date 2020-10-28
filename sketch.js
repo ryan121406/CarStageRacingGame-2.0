eff370d0e6093cfd8eff5578667e846e89d3ce1a
Ryan Kim <ryanckim14@icloud.com>
2020-10-27 18:51:56 -0700

    Added Images

--- sketch.js
+++ sketch.js
@@ -10,6 +10,16 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+function preload(){
+
+groundImage = loadImage("../images/ground.png")
+trackImage = loadImage("../images/track.jpg")
+car1Image = loadImage("../images/car1.png")
+car2Image = loadImage("../images/car2.png")
+car3Image = loadImage("../images/car3.png")
+car4Image = loadImage("../images/car4.png")
+
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +37,11 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+
+  game.end();
+  
+
   }
 }
